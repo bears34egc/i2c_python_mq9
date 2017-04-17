4e19bd0ad9f5a8fd5d4f094590ebbfd3bff82e5a
bears34egc <ericgcoker@gmail.com>
2017-04-16 20:47:44 -0500

    change_again to test read_block_data error

--- src/sensor.py
+++ src/sensor.py
@@ -16,22 +16,32 @@ pb = Pushbullet('o.EZiuid3oCjU9aUM153BRs41bHYglKNee')
 
 # busNumber = int(os.getenv("I2C_BUS"))
 
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(0)
 sleep(.3)
-
-# ADC121C_MQ9 address, 0x50(80)
-# Read data back from 0x00(00), 2 bytes
-# raw_adc MSB, raw_adc LSB
-data = bus.read_i2c_block_data(0x50, 0x00, 2)
-sleep(.3)
-# Convert the data to 12-bits
-raw_adc = (data[0] & 0x0F) * 256 + data[1]
-ppm = (1000.0 / 4096.0) * raw_adc + 10
-
-
+bus.write_byte(0x48, 0)
 
 while True:
-    # print "Carbon Monoxide Concentration : %.2f ppm" %ppm
+    data = bus.read_byte(0x48)
+    sleep(.3)
+    # Convert the data to 12-bits
+    raw_adc = (data[0] & 0x0F) * 256 + data[1]
+    ppm = (1000.0 / 4096.0) * raw_adc + 10
     push = pb.push_note("the ppm is ", ppm)
     sleep(500)
+
+# # ADC121C_MQ9 address, 0x50(80)
+# # Read data back from 0x00(00), 2 bytes
+# # raw_adc MSB, raw_adc LSB
+# data = bus.read_i2c_block_data(0x50, 0x00, 2)
+# sleep(.3)
+# # Convert the data to 12-bits
+# raw_adc = (data[0] & 0x0F) * 256 + data[1]
+# ppm = (1000.0 / 4096.0) * raw_adc + 10
+
+
+
+# while True:
+#     # print "Carbon Monoxide Concentration : %.2f ppm" %ppm
+#     push = pb.push_note("the ppm is ", ppm)
+#     sleep(500)
 
